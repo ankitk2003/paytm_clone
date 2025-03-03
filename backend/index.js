0e1503620f077e827a491ea2d4cd1a066afb736e
Ankit Kumar <ankit1063.be22@chitkarauniversity.edu.in>
2025-03-04 01:02:29 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,25 @@
-import express from "express"
+import express from "express";
 import mongoose from "mongoose";
-import cors from "cors"
-const userRouter = require("./routes/user");
+import cors from "cors";
+import userRouter from "./routes/user.js";  // Add .js extension for ES Modules
+import accountRouter from "./routes/account.js"; 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-const accountRouter=require("./routes/account");
+
 app.use("/api/v1/user", userRouter);
-app.use("/api/v1/account",accountRouter)
+app.use("/api/v1/account", accountRouter);
+
 async function main() {
   await mongoose.connect(
     "mongodb+srv://ankitdevx1808:ankit2003@cluster0.wlxth.mongodb.net/paytm"
   );
   console.log("db connected");
-  await app.listen(3000, () => {
+
+  app.listen(3000, () => {
     console.log("server started");
   });
 }
+
 main();
