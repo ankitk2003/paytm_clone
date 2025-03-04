36e12d8ffb6879f439dc7180bda0856afd5dde34
Ankit Kumar <ankit1063.be22@chitkarauniversity.edu.in>
2025-03-04 14:58:07 +0530

    made change in api fetch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { all } from 'axios'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    host:true,
+    allowedHosts:"all"
+  },
   plugins: [react(), tailwindcss()
   ],
 
