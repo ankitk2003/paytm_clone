a60b75b29fa957af9866008428fe2ae9ac4eb509
Ankit Kumar <ankit1063.be22@chitkarauniversity.edu.in>
2025-03-05 09:52:53 +0530

    fourth commit

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -41,6 +41,7 @@ const Signup = () => {
     return (
         <div className="bg-slate-300 h-screen flex justify-center">
             <div className="flex flex-col justify-center">
+                this is signup page
                 <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                     <Heading label={"Sign up"} />
                     <SubHeading label={"Enter your information to create an account"} />
