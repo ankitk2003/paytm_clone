b22622ec9b41cd64cc393f29732dda8f45a4dfb0
Ankit Kumar <ankit1063.be22@chitkarauniversity.edu.in>
2025-03-05 10:31:19 +0530

    checking ci

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -41,10 +41,11 @@ const Signup = () => {
     return (
         <div className="bg-slate-300 h-screen flex justify-center">
             <div className="flex flex-col justify-center">
-                this is signup page
                 <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                     <Heading label={"Sign up"} />
                     <SubHeading label={"Enter your information to create an account"} />
+                    <InputBox onChange={(e) => setFirstName(e.target.value) } placeholder="John" label={"First Name"} />
+
                     <InputBox onChange={(e) => setFirstName(e.target.value) } placeholder="John" label={"First Name"} />
                     <InputBox onChange={(e) => setLastName(e.target.value)} placeholder="Doe" label={"Last Name"} />
                     <InputBox onChange={(e) => setUsername(e.target.value)} placeholder="harkirat@gmail.com" label={"Email"} />
